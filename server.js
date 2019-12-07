35442a49f36e1b8f868844cef319fb1895a3d901
benjamin-t-wilson <benjiimanwillson@gmail.com>
2019-12-07 10:24:07 -0700

    added session store

--- server.js
+++ server.js
@@ -1,26 +1,34 @@
 const express = require("express");
-const helmet = require("helmet")
-const session = require("express-session")
-const knexSessionStore = require("connect-section-knex")(session)
+const helmet = require("helmet");
+const session = require("express-session");
+const knexSessionStore = require("connect-section-knex")(session);
 const apiRouter = require("./routes/api/apiRoute.js");
 
 const server = express();
 
 const sessionConfig = {
-    name: "babyYoda",
-    secret: "is a puppet in some scenes, CGI in others",
-    cookie: {
-        maxAge: 1000 * 30, // max age is in miliseconds, 1000ms is 1s, * 30 makes it good for 30 seconds
-        secure: false, // set to true for production
-        httpOnly: true, // cookie can only be transferred using http methods and is inaccessible via JS
-    },
-    resave: false, // don't save a new cookie if it hasn't changed
-    saveUninitialized: true, // set to false for production. People must accept cookies
-}
+  name: "babyYoda",
+  secret: "is a puppet in some scenes, CGI in others",
+  cookie: {
+    maxAge: 1000 * 30, // max age is in miliseconds, 1000ms is 1s, * 30 makes it good for 30 seconds
+    secure: false, // set to true for production
+    httpOnly: true // cookie can only be transferred using http methods and is inaccessible via JS
+  },
+  resave: false, // don't save a new cookie if it hasn't changed
+  saveUninitialized: true, // set to false for production. People must accept cookies
 
-server.use(helmet)
+  store: new knexSessionStore({
+    knex: require("./data/dbConfig.js"),
+    tablename: "sessions",
+    sidfieldname: "sid",
+    createtable: true,
+    clearInterval: 1000 * 60 * 60
+  })
+};
+
+server.use(helmet);
 server.use(express.json());
-server.use(session(sessionConfig))
+server.use(session(sessionConfig));
 server.use("/api", apiRouter);
 
 module.exports = server;
